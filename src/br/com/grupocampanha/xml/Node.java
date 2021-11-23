53b272c66b256dfda1af44449a8c7d2a2692539a
victor <jfc@DESKTOP-RQLDHIH>
2021-11-23 18:09:01 -0300

    aadawd

--- src/br/com/grupocampanha/xml/Node.java
+++ src/br/com/grupocampanha/xml/Node.java
@@ -1,11 +1,11 @@
 package br.com.grupocampanha.xml;
 
-
 import br.com.grupocampanha.xml.exceptions.InsertNodeException;
 import br.com.grupocampanha.xml.exceptions.InsertNodeValueException;
 import java.util.HashMap;
 import java.util.Map;
 import br.com.grupocampanha.xml.interfaces.Find;
+import java.util.function.Consumer;
 
 public class Node {
 
@@ -24,94 +24,151 @@ public class Node {
         return nome;
     }
 
-    public void mudarNome(String nome) {
+    public Node mudarNome(String nome) {
         this.nome = nome;
+        return this;
     }
 
-    public void adicionarNode(Node node) throws InsertNodeException {
-        if (valor != null) {
-            throw new InsertNodeException();
-        }
+    public Node add(Node node) {
+        if (valor != null)
+            new InsertNodeException().printStackTrace();
         nodes = redimensionarArray(nodes, nodes.length + 1);
         nodes[nodes.length - 1] = node;
         tamanho++;
+        return this;
     }
 
-    public boolean removerNode(Node node){
+    public Node remove(Node node) {
 
-        for (int i = 0; i < nodes.length; i++) {
+        for (int i = 0; i < nodes.length; i++)
             if (nodes[i].equals(node)) {
-                for (int k = i; k < nodes.length - 1; k++) {
+                for (int k = i; k < nodes.length - 1; k++)
                     nodes[k] = nodes[k + 1];
-                }
                 nodes = (Node[]) redimensionarArray(nodes, nodes.length - 1);
                 tamanho--;
-                return true;
+                break;
             }
-        }
-       return false;
+        return this;
     }
 
-    public boolean removerNode(int index){
-        if (index > nodes.length - 1 || index < 0) {
-         return false;
-        }
+    public Node remove(int index) {
+        if (index > nodes.length - 1 || index < 0)
+            return this;
 
-        for (int i = index; i < nodes.length - 1; i++) {
+        for (int i = index; i < nodes.length - 1; i++)
             nodes[i] = nodes[i + 1];
-        }
         nodes = (Node[]) redimensionarArray(nodes, nodes.length - 1);
         tamanho--;
-        return true;
+        return this;
     }
 
-    public void setValor(String valor) throws InsertNodeValueException {
-        if (nodes.length > 0) {
-            throw new InsertNodeValueException();
-        }
+    public Node add(String valor) {
+        if (nodes.length > 0)
+            new InsertNodeValueException().printStackTrace();
         this.valor = valor;
+        return this;
     }
 
     public String getValor() {
         return valor;
     }
-
+    public Node forEach(Consumer<Node> a){
+    for(Node n : nodes)
+        a.accept(n);
+    
+    return this;
+    }
     public String getPropriedade(String propriedade) {
         return propriedades.get(propriedade);
     }
 
-    public String propriedadeAt(int index) {
+    public String getPropriedade(int index) {
         return (String) propriedades.values().toArray()[index];
     }
 
-    public Node nodeAt(int index)   {
-            return nodes[index];
+    public Node get(int index) {
+        return nodes[index];
     }
 
-    public void adicionarPropriedade(String propriedade, String Valor) {
+    public Node get(String path) {
+        return getByString(this, path);
+
+    }
+
+    private Node getByString(Node nodePrincipal, String path) {
+        class PS {
+
+            int initNextNode;
+            int nodePosition;
+            String nomeNode;
+
+            public PS(String path) {
+                path = path.substring(path.indexOf("/")+1);
+                initNextNode = path.indexOf("/")+1;
+                String frag = path.substring(0, path.indexOf("/"));
+                nodePosition = nodeIndex(frag);
+                if (nodePosition == -1)
+                    nomeNode = frag;
+                else
+                    nomeNode = frag.substring(0, frag.indexOf("["));
+            }
+
+            private int nodeIndex(String frag) {
+                int initIndex;
+                int endIndex;
+                initIndex = frag.indexOf("[");
+                if (initIndex == -1)
+                    return -1;
+                int qtdchar = frag.substring(0, initIndex + 1).length();
+                endIndex = frag.substring(initIndex + 1).indexOf("]");
+                if (endIndex == -1)
+                    return -1;
+
+                endIndex += qtdchar;
+
+                String numeros = frag.substring(initIndex+1, endIndex);
+                if (!numeros.chars().allMatch(Character::isDigit))
+                    return -1;
+                return Integer.parseInt(numeros);
+            }
+
+        }
+        PS ps = new PS(path);
+       Node[] nods = nodePrincipal.findAll(f->f.getNome().equals(ps.nomeNode));
+       Node node; 
+       if(nods.length >1)
+           if(ps.nodePosition == -1)
+               return null;
+           else
+               node  = nods[ps.nodePosition];
+       else if(nods.length == 0)
+           return null;
+       else
+           node = nods[0];
+           
+       if(!path.substring(ps.initNextNode+1).isEmpty())
+           return getByString(node, path.substring(ps.initNextNode));
+       else
+           return node;
+    }
+
+    public Node adicionarPropriedade(String propriedade, String Valor) {
         this.propriedades.put(propriedade, Valor);
         tamanhoAtributo++;
+        return this;
     }
 
-    public boolean removerPropriedade(String propriedade) {
-        Object[] keys = this.propriedades.keySet().toArray();
+    public Node removerPropriedade(String propriedade) {
 
-        boolean existe = false;
-        for (Object o : keys) {
-            if (((String) o).equals(propriedade)) {
-                existe = true;
-            }
-        }
         this.propriedades.remove(propriedade);
         tamanhoAtributo--;
-        return existe;
+        return this;
     }
 
     private Node[] redimensionarArray(Node[] array, int novoTamanho) {
         Node[] novoArray = new Node[novoTamanho];
-        for (int i = 0; i < (novoTamanho > array.length ? array.length : novoTamanho   ); i++) {
+        for (int i = 0; i < (novoTamanho > array.length ? array.length : novoTamanho); i++)
             novoArray[i] = array[i];
-        }
         return novoArray;
     }
 
@@ -127,75 +184,66 @@ public class Node {
         if (propriedades != null) {
             Object[] chaves = propriedades.keySet().toArray();
             Object[] valores = propriedades.values().toArray();
-            for (int i = chaves.length - 1; i >= 0; i--) {
-                if (((String) valores[i]).contains("\"")) {
+            for (int i = chaves.length - 1; i >= 0; i--)
+                if (((String) valores[i]).contains("\""))
                     sb.append(" " + chaves[i] + "='" + valores[i] + "'");
-                } else {
+                else
                     sb.append(" " + chaves[i] + "=\"" + valores[i] + "\"");
-                }
-            }
         }
 
         if (valor == null && nodes.length == 0) {
             sb.append("/>");
             return sb.toString();
         }
-        if (nome.toLowerCase().equals("?xml")) {
+        if (nome.toLowerCase().equals("?xml"))
             sb.append("?>");
-        } else {
+        else
             sb.append(">");
-        }
 
         if (valor != null) {
             sb.append(valor);
-            if (!nome.toLowerCase().equals("?xml")) {
+            if (!nome.toLowerCase().equals("?xml"))
                 sb.append("</" + nome + ">");
-            }
         } else {
 
-            for (int i = 0; i < nodes.length; i++) {
+            for (int i = 0; i < nodes.length; i++)
 
                 sb.append("\n" + gerarTabulacao(tabulacao) + nodes[i].toStringImplementation(tabulacao + 1));
-            }
 
-            if (!nome.toLowerCase().equals("?xml")) {
+            if (!nome.toLowerCase().equals("?xml"))
                 sb.append("\n" + gerarTabulacao(tabulacao - 1) + "</" + nome + ">");
-            }
         }
         return sb.toString();
     }
 
     private String gerarTabulacao(int qtd) {
         String tabulacao = "";
-        for (int i = 0; i < qtd; i++) {
+        for (int i = 0; i < qtd; i++)
             tabulacao = tabulacao + "   ";
-        }
         return tabulacao;
     }
 
     public Node find(Find f) {
         Node result = findImplementation(f, this);
-        if (f.find(result)) {
+        if (f.find(result))
             return result;
-        }
         return null;
     }
 
     private Node findImplementation(Find f, Node node) {
 
-        if (f.find(node)) {
+        if (f.find(node))
             return node;
-        }
 
         for (int i = 0; i < node.nodes.length; i++) {
-        	Node n = findImplementation(f, node.nodes[i]);
-        	if(n != null && f.find(n))
-        		return n;
+            Node n = findImplementation(f, node.nodes[i]);
+            if (n != null && f.find(n))
+                return n;
         }
         return null;
     }
 
-    public Node[] findNodes(Find f) {
+    public Node[] findAll(Find f) {
 
         return findNodesImplementation(f, this);
     }
@@ -208,9 +256,8 @@ public class Node {
             nodes[0] = node;
         }
 
-        for (int i = 0; i < node.nodes.length; i++) {
-                nodes = Utilitario.juntarNodes(nodes, findNodesImplementation(f, node.nodes[i]));
-        }
+        for (int i = 0; i < node.nodes.length; i++)
+            nodes = Utilitario.juntarNodes(nodes, findNodesImplementation(f, node.nodes[i]));
         return nodes;
     }
 
@@ -219,11 +266,9 @@ public class Node {
     }
 
     public boolean contains(Find f) {
-        for (int i = 0; i < nodes.length; i++) {
-            if (f.find(nodes[i])) {
+        for (int i = 0; i < nodes.length; i++)
+            if (f.find(nodes[i]))
                 return true;
-            }
-        }
         return false;
 
     }
@@ -237,26 +282,18 @@ public class Node {
 class Utilitario {
 
     public static boolean contemCaracterEspecial(String string) {
-        for (char c : string.toCharArray()) {
-            if (!Character.isLetter(c) && !Character.isDigit(c)) {
+        for (char c : string.toCharArray())
+            if (!Character.isLetter(c) && !Character.isDigit(c))
                 return true;
-
-            }
-
-        }
         return false;
     }
 
     public static boolean contemCaracterEspecial(String string, boolean permirtir, char... caracter) {
-        for (char c : string.toCharArray()) {
-            if (!Character.isLetter(c) && !Character.isDigit(c)) {
-                for (char i : caracter) {
-                    if (i == c && !permirtir) {
+        for (char c : string.toCharArray())
+            if (!Character.isLetter(c) && !Character.isDigit(c))
+                for (char i : caracter)
+                    if (i == c && !permirtir)
                         return true;
-                    }
-                }
-            }
-        }
         return false;
     }
 
--- src/br/com/grupocampanha/xml/XML.java
+++ src/br/com/grupocampanha/xml/XML.java
@@ -1,13 +1,13 @@
 package br.com.grupocampanha.xml;
 
-
 import br.com.grupocampanha.xml.exceptions.InsertNodeException;
 import br.com.grupocampanha.xml.exceptions.InsertNodeValueException;
-
 import br.com.grupocampanha.xml.exceptions.UnformattedXmlException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
@@ -18,166 +18,182 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 public class XML {
-	public static Document parse(File arquivo) throws IOException, InsertNodeException, InsertNodeValueException, UnformattedXmlException {
-		String xml = removerEscapes(new String(Files.readAllBytes(arquivo.toPath())));
-		Document documento = new Document(arquivo.getAbsolutePath());
-		Node nodePai = new Node("");
-		if (!subNode(xml, nodePai).equals(""))
-			throw new UnformattedXmlException(); 
-	
-			documento.node = nodePai.nodeAt(0);
 
-		return documento;
-	}
+    public static Document parse(File arquivo) throws IOException, UnformattedXmlException {
+        String xml = removerEscapes(new String(Files.readAllBytes(arquivo.toPath())));
+        Document documento = new Document(arquivo.getAbsolutePath());
+        Node nodePai = new Node("");
 
-	public static Document parse(String xml) throws  InsertNodeException, InsertNodeValueException, UnformattedXmlException {
-		xml = removerEscapes(xml);
-		Document documento = new Document("xml.xml");
-		Node nodePai = new Node("");
-		if (!subNode(xml, nodePai).equals(""))
-			throw new UnformattedXmlException(); 
+        if (!subNode(excluirComentarios(xml), nodePai).equals(""))
+            throw new UnformattedXmlException();
 
-			documento.node = nodePai.nodeAt(0);
-	
-		return documento;
+        documento.node = nodePai.get(0);
+        return documento;
+    }
 
-	}
+    public static Document parse(String xml) throws UnformattedXmlException {
+        xml = removerEscapes(xml);
+        Document documento = new Document("xml.xml");
+        Node nodePai = new Node("");
+        if (!subNode(xml, nodePai).equals(""))
+            throw new UnformattedXmlException();
+        documento.node = nodePai.get(0);
 
-	public static Document parse(org.w3c.dom.Document doc) throws TransformerConfigurationException, TransformerException, IOException, InsertNodeException, InsertNodeValueException, UnformattedXmlException {
-		TransformerFactory tranFactory = TransformerFactory.newInstance();
-		Transformer aTransformer = tranFactory.newTransformer();
-		Source src = new DOMSource(doc);
-		File file = new File("xml.xml");
-		Result dest = new StreamResult(file);
-		aTransformer.transform(src, dest);
-		return parse(file);
-	}
+        return documento;
 
-	private static Node criarNode(String fragmento)  {
-		String nome = "";
-		Node node = null;
-		boolean primeiraLetra = false;
-		String nomeProPriedade = "";
-		boolean nomeFinalizado = false;
-		boolean valor = false;
-		boolean aspasUnicas = false;
-		boolean primeiraAspa = false;
-		String valorPropriedade = "";
-		for (int i = 1; i < fragmento.length(); i++) {
-			char c = fragmento.charAt(i);
-			if (node == null) {
-				if (!primeiraLetra && c != ' ' && c != '/' && c != '<') {
-					primeiraLetra = true;
-					nome = Character.toString(c);
-				} else if (primeiraLetra && (c == ' ' || c == '/' || c == '>')) {
-					node = new Node(nome);
-				} else if (primeiraLetra && c != ' ') {
-					nome = nome + Character.toString(c);
-				} 
-			} else if (c == '=') {
-				nomeFinalizado = true;
-			} else if (c != ' ' && c != '"' && c != '>' && c != '\'' && !nomeFinalizado) {
-				nomeProPriedade = nomeProPriedade + Character.toString(c);
-			} else {
-				if (c == '>' && valorPropriedade.equals("") && nomeProPriedade.equals(""))
-					return node; 
-				if (nomeFinalizado && c == '\'' && valorPropriedade.equals("") && !primeiraAspa) {
-					aspasUnicas = true;
-					valor = true;
-					primeiraAspa = true;
-				} else if (nomeFinalizado && c == '"' && valorPropriedade.equals("") && !primeiraAspa) {
-					aspasUnicas = false;
-					valor = true;
-					primeiraAspa = true;
-				} else if (!valorPropriedade.equals("") && aspasUnicas && c == '\'') {
-					node.adicionarPropriedade(nomeProPriedade.trim(), valorPropriedade);
-					nomeProPriedade = "";
-					valorPropriedade = "";
-					aspasUnicas = false;
-					primeiraAspa = false;
-					nomeFinalizado = false;
-				} else if (!valorPropriedade.equals("") && !aspasUnicas && c == '"') {
-					node.adicionarPropriedade(nomeProPriedade.trim(), valorPropriedade);
-					nomeProPriedade = "";
-					valorPropriedade = "";
-					aspasUnicas = false;
-					primeiraAspa = false;
-					nomeFinalizado = false;
-				} else if (valor && primeiraAspa) {
-					valorPropriedade = valorPropriedade + Character.toString(c);
-				} 
-			} 
-		} 
-		return node;
-	}
+    }
 
-	private static String subNode(String fragmento, Node node) throws InsertNodeException, InsertNodeValueException {
-		if (!fragmento.equals("")) {
-			String nodeString = "";
-			int indexAbridor = fragmento.indexOf('<');
-			int indexFechador = fragmento.indexOf('>');
-			nodeString = fragmento.substring(indexAbridor, indexFechador + 1);
-			fragmento = fragmento.substring(indexFechador + 1).trim();
-			if (!nodeFechamento(nodeString)) {
-				Node tempNode = criarNode(nodeString);
-				node.adicionarNode(tempNode);
-				//ignora comentarios
-				while(fragmento.length() >4 &&fragmento.startsWith("<!--")){
-					fragmento = fragmento.substring(fragmento.indexOf(">")+1);
-				}
+    public static Document parse(org.w3c.dom.Document doc) throws UnformattedXmlException {
+        TransformerFactory tranFactory = TransformerFactory.newInstance();
+    
+        try {
+              Transformer aTransformer = tranFactory.newTransformer();
+                      Source src = new DOMSource(doc);
+        File file = new File("xml.xml");
+        Result dest = new StreamResult(file);
+        aTransformer.transform(src, dest);
+         return parse(file);
+        } catch ( IOException | TransformerException ex) {
+           ex.printStackTrace();
+        }
+        return null;
+    }
 
-				int indexAbertura = fragmento.indexOf("<");
-				int indexFechamento = fragmento.indexOf(">");
-				String inicioNode = fragmento.substring(0, 2);
+    private static Node criarNode(String fragmento) {
 
-				while (inicioNode.equals("<!")) {
-					indexAbertura = indexAbertura + 1 + fragmento.substring(indexAbertura + 1).indexOf('<');
-					indexFechamento = indexFechamento + 1 + fragmento.substring(indexFechamento + 1).indexOf('>');
-					inicioNode = fragmento.substring(indexAbertura, indexAbertura + 2);
-				} 
-				if (indexAbertura != 0) {
-					tempNode.setValor(fragmento.substring(0, indexAbertura));
-					return fragmento.substring(indexFechamento + 1);
-				} 
-				if (nodeFechamentoComPropriedade(nodeString))
-					return fragmento; 
-				String nomeProximoNode = fragmento.substring(fragmento.indexOf("<") + 1, fragmento.indexOf(">")).replace("/", "").trim();
-				try {
-					while (!nomeProximoNode.equals(tempNode.getNome())) {
-						fragmento = subNode(fragmento, tempNode);
-						nomeProximoNode = fragmento.substring(fragmento.indexOf("<") + 1, fragmento.indexOf(">")).replace("/", "").trim();
-					} 
-				} catch (StringIndexOutOfBoundsException e) {
-					return fragmento;
-				} 
-				fragmento = fragmento.substring(fragmento.indexOf(">") + 1);
-			} 
-		} 
-		if (fragmento.replace(" ", "").replace(" ", "").equals(""))
-			fragmento = ""; 
-		return fragmento;
-	}
+        Node node = null;
+        String nomeProPriedade = "";
+        String nome = "";
+        String valorPropriedade = "";
+        boolean primeiraLetra = false;
+        boolean nomeFinalizado = false;
+        boolean valor = false;
+        boolean aspasUnicas = false;
+        boolean primeiraAspa = false;
 
-	private static boolean nodeFechamentoComPropriedade(String fragmento) {
-		fragmento = fragmento.replace(" ", "");
-		if (fragmento.substring(fragmento.length() - 2).equals("/>"))
-			return true; 
-		return false;
-	}
+        for (int i = 1; i < fragmento.length(); i++) {
+            char c = fragmento.charAt(i);
+            if (node == null) {
+                if (!primeiraLetra && c != ' ' && c != '/' && c != '<') {
+                    primeiraLetra = true;
+                    nome = Character.toString(c);
+                } else if (primeiraLetra && (c == ' ' || c == '/' || c == '>'))
+                    node = new Node(nome);
+                else if (primeiraLetra && c != ' ')
+                    nome = nome + Character.toString(c);
+            } else if (c == '=')
+                nomeFinalizado = true;
+            else if (c != ' ' && c != '"' && c != '>' && c != '\'' && !nomeFinalizado)
+                nomeProPriedade = nomeProPriedade + Character.toString(c);
+            else {
+                if (c == '>' && valorPropriedade.equals("") && nomeProPriedade.equals(""))
+                    return node;
+                if (nomeFinalizado && c == '\'' && valorPropriedade.equals("") && !primeiraAspa) {
+                    aspasUnicas = true;
+                    valor = true;
+                    primeiraAspa = true;
+                } else if (nomeFinalizado && c == '"' && valorPropriedade.equals("") && !primeiraAspa) {
+                    aspasUnicas = false;
+                    valor = true;
+                    primeiraAspa = true;
+                } else if (!valorPropriedade.equals("") && aspasUnicas && c == '\'') {
+                    node.adicionarPropriedade(nomeProPriedade.trim(), valorPropriedade);
+                    nomeProPriedade = "";
+                    valorPropriedade = "";
+                    aspasUnicas = false;
+                    primeiraAspa = false;
+                    nomeFinalizado = false;
+                } else if (!valorPropriedade.equals("") && !aspasUnicas && c == '"') {
+                    node.adicionarPropriedade(nomeProPriedade.trim(), valorPropriedade);
+                    nomeProPriedade = "";
+                    valorPropriedade = "";
+                    aspasUnicas = false;
+                    primeiraAspa = false;
+                    nomeFinalizado = false;
+                } else if (valor && primeiraAspa)
+                    valorPropriedade = valorPropriedade + Character.toString(c);
+            }
+        }
+        return node;
+    }
 
-	private static boolean nodeFechamento(String fragmento) {
-		if (fragmento.replace(" ", "").substring(0, 2).equals("</"))
-			return true; 
-		return false;
-	}
+    private static String excluirComentarios(String texto) {
+        //ignora comentarios
+        while (true) {
+            int indexAbertura = texto.indexOf("<!--");
+            int indexFechamento = texto.indexOf("-->");
+            if (indexAbertura == -1 || indexFechamento == -1)
+                break;
+            texto = texto.substring(0, indexAbertura) + texto.substring(indexFechamento + 3);
+        }
+        return texto;
+    }
 
-	private static String removerEscapes(String string) {
-		String novaString = string;
-		novaString = novaString.replace("\t", "");
-		novaString = novaString.replace("\b", "");
-		novaString = novaString.replace("\n", "");
-		novaString = novaString.replace("\r", "");
-		novaString = novaString.replace("\f", "");
-		return novaString;
-	}
+    private static String subNode(String fragmento, Node node)  {
+        fragmento = fragmento.trim();
+        if (!fragmento.equals("")) {
+
+            String nodeString = "";
+            int indexAbridor = fragmento.indexOf('<');
+            int indexFechador = fragmento.indexOf('>');
+            nodeString = fragmento.substring(indexAbridor, indexFechador + 1);
+            fragmento = fragmento.substring(indexFechador + 1).trim();
+            if (!nodeFechamento(nodeString)) {
+
+                Node tempNode = criarNode(nodeString);
+                node.add(tempNode);
+
+                fragmento = fragmento.trim();
+                int indexAbertura = fragmento.indexOf("<");
+                int indexFechamento = fragmento.indexOf(">");
+                String inicioNode = fragmento.substring(0, 2);
+
+                while (inicioNode.equals("<!")) {
+                    indexAbertura = indexAbertura + 1 + fragmento.substring(indexAbertura + 1).indexOf('<');
+                    indexFechamento = indexFechamento + 1 + fragmento.substring(indexFechamento + 1).indexOf('>');
+                    inicioNode = fragmento.substring(indexAbertura, indexAbertura + 2);
+                }
+                if (indexAbertura != 0) {
+                    tempNode.add(fragmento.substring(0, indexAbertura));
+                    return fragmento.substring(indexFechamento + 1);
+                }
+                if (nodeFechamentoComPropriedade(nodeString))
+                    return fragmento;
+                String nomeProximoNode = fragmento.substring(fragmento.indexOf("<") + 1, fragmento.indexOf(">")).replace("/", "").trim();
+                try {
+                    while (!nomeProximoNode.equals(tempNode.getNome())) {
+                        fragmento = subNode(fragmento, tempNode);
+                        nomeProximoNode = fragmento.substring(fragmento.indexOf("<") + 1, fragmento.indexOf(">")).replace("/", "").trim();
+                    }
+                } catch (StringIndexOutOfBoundsException e) {
+                    return fragmento;
+                }
+                fragmento = fragmento.substring(fragmento.indexOf(">") + 1);
+            }
+        }
+        if (fragmento.replace(" ", "").replace(" ", "").equals(""))
+            fragmento = "";
+        return fragmento;
+    }
+
+    private static boolean nodeFechamentoComPropriedade(String fragmento) {
+        fragmento = fragmento.replace(" ", "");
+       return (fragmento.substring(fragmento.length() - 2).equals("/>"));
+          
+    }
+
+    private static boolean nodeFechamento(String fragmento) {
+        return fragmento.replace(" ", "").substring(0, 2).equals("</");
+
+    }
+
+    private static String removerEscapes(String string) {
+        String novaString = string;
+        novaString = novaString.replace("\t", "");
+        novaString = novaString.replace("\b", "");
+        novaString = novaString.replace("\n", "");
+        novaString = novaString.replace("\r", "");
+        novaString = novaString.replace("\f", "");
+        return novaString;
+    }
 }
